2f3a16e166cb137553793b17948f1fc659cfa50b
Benjamin Z <benzhai21@gmail.com>
2022-02-16 11:34:25 -0700

    magic 8 ball done

---
+++ script.js
@@ -0,0 +1,42 @@
+// MAGIC 8 BALL PROJECT
+
+// ELEMENT NAMES
+let ansEl=document.getElementById("ans");
+
+// EVENT LISTENER
+document.getElementById("ball").addEventListener("click", answerq)
+
+// EVENT FUNCTION
+function answerq(){
+    // Input
+    let quest = document.getElementById("ask").value;
+
+   
+    
+
+    // Check Question
+    if(quest === "Does a magic 8 ball actually work?"){
+        ansEl.innerHTML = "HOW DARE YOU DOUBT ME"
+    } else if(quest === "Is JavaScript awesome?"){
+        ansEl.innerHTML = "OF COURSE IT IS"
+    } else {
+        let ranum = Math.random();
+        if(ranum <0.2){
+        console.log(ranum)
+        ansEl.innerHTML = "Without a Doubt."
+    } else if (ranum < 0.4){
+        console.log(ranum)
+        ansEl.innerHTML = "As I see it, yes."
+    } else if (ranum < 0.6){
+        console.log(ranum)
+        ansEl.innerHTML = "Concentrate and ask again."
+    } else if (ranum < 0.8){
+        console.log(ranum)
+        ansEl.innerHTML = "Don't count on it."
+    } else {
+        console.log(ranum)
+        ansEl.innerHTML = "Outlook not so good."
+    }
+}
+
+}
